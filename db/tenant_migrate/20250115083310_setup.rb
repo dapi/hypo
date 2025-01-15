32b5e53187532f67ff86bad13d058b9d88e36a8e
Danil Pismenny <danil@brandymint.ru>
2025-01-15 11:56:44 +0300

    minor

--- db/tenant_migrate/20250115083310_setup.rb
+++ db/tenant_migrate/20250115083310_setup.rb
@@ -3,6 +3,6 @@
 class Setup < ActiveRecord::Migration[5.1]
   def up
     # Source is https://wiki.postgresql.org/wiki/Clone_schema
-    execute Rails.root.join('db/clone_schema.sql').read
+    execute Rails.root.join('db/functions/clone_schema.sql').read
   end
 end

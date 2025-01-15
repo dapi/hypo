2e258c8fb5d9491f14be09f692420c07964816dc
Danil Pismenny <danil@brandymint.ru>
2025-01-15 11:56:11 +0300

    Add clone schema function

---
+++ db/tenant_migrate/20250115083310_setup.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Setup < ActiveRecord::Migration[5.1]
+  def up
+    # Source is https://wiki.postgresql.org/wiki/Clone_schema
+    execute Rails.root.join('db/clone_schema.sql').read
+  end
+end

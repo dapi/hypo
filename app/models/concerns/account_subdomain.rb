dda4ceb38d83a588c07f29663917929b2924f710
Danil Pismenny <danil@brandymint.ru>
2025-01-16 21:51:00 +0300

    lint

--- app/models/concerns/account_subdomain.rb
+++ app/models/concerns/account_subdomain.rb
@@ -2,6 +2,7 @@
 
 module AccountSubdomain
   extend ActiveSupport::Concern
+  SUBDOMAIN_LENGTH = 8
   SUBDOMAIN_SYMBOLS = "1234567890#{('a'..'z').to_a.join}".freeze
   SUBDOMAIN_PREFIX_SYMBOLS = ('a'..'z').to_a.freeze
 
@@ -13,6 +14,6 @@ module AccountSubdomain
 
   def generate_subdomain
     SUBDOMAIN_PREFIX_SYMBOLS.sample +
-      Nanoid.generate size: 10, alphabet: SUBDOMAIN_SYMBOLS
+      Nanoid.generate(size: SUBDOMAIN_LENGTH-1, alphabet: SUBDOMAIN_SYMBOLS)
   end
 end

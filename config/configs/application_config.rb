6cd0a0da7cd5509ac9dda86a2490483e9bde500f
Danil Pismenny <danil@brandymint.ru>
2025-01-16 21:22:24 +0300

    Add ApplicationConfig

---
+++ config/configs/application_config.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Base class for application config classes
+class ApplicationConfig < Anyway::Config
+  env_prefix :vilna
+  attr_config(
+    app_title: 'Vilna',
+    host: 'localhost',
+    protocol: 'https',
+    # sidekiq_redis_url: 'redis://localhost:6379/0',
+    bot_token: '',
+    bot_username: '',
+  )
+
+  class << self
+    # Make it possible to access a singleton config instance
+    # via class methods (i.e., without explicitly calling `instance`)
+    delegate_missing_to :instance
+
+    private
+
+    # Returns a singleton config instance
+    def instance
+      @instance ||= new
+    end
+  end
+end

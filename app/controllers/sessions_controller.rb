1769965431f0719a0985a8cc4582e3b9ee5a3689
Danil Pismenny <danil@brandymint.ru>
2025-01-16 22:13:01 +0300

    lint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,17 +9,8 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
-      start_new_session_for user
-      redirect_to after_authentication_url
-    else
-      redirect_to new_session_path, alert: "Try another email address or password."
-    end
-  end
-
   def destroy
-    terminate_session
-    redirect_to new_session_path
+    logout
+    redirect_to root_url(format: :html), status: :see_other, notice: t('flash.buy')
   end
 end

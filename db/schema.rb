88751b225ad13e24354635f44a44465ab7f7006c
Danil Pismenny <danil@brandymint.ru>
2025-01-16 21:47:56 +0300

    Add TelegramUser and Membership

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_16_184344) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_16_184741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,24 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_16_184344) do
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_accounts_on_owner_id"
     t.index ["subdomain"], name: "index_accounts_on_subdomain", unique: true
+  end
+
+  create_table "memberships", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "account_id", null: false
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_memberships_on_account_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "telegram_users", id: :string, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -35,10 +53,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_16_184344) do
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
     t.string "last_login_from_ip_address"
+    t.bigint "telegram_user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["telegram_user_id"], name: "index_users_on_telegram_user_id"
   end
 
   add_foreign_key "accounts", "users", column: "owner_id"
+  add_foreign_key "memberships", "accounts"
+  add_foreign_key "memberships", "users"
 end

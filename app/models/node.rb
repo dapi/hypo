9293f73a6c4233a589acaef108ff10509907a57d
Danil Pismenny <danil@brandymint.ru>
2025-01-17 17:23:55 +0300

    Add state machine to node

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,28 @@
 class Node < ApplicationRecord
   belongs_to :account
+
+  state_machine initial: "initiated" do
+    event :start do
+      transition initiated: :starting
+    end
+
+    event :started do
+      transition starting: :processing
+    end
+
+    event :failed do
+      transition starting: :failed_to_start
+      transition finishing: :failed_to_finish
+    end
+
+    event :finish do
+      transition failed_to_start: :finishing
+      transition failed_to_finish: :finishing
+      transition processing: :finishing
+    end
+
+    event :finished do
+      transition finishing: :finished
+    end
+  end
 end

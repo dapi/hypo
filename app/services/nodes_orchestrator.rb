1cbb9ba1f19150c59e29575b277cba2d99c295b5
Danil Pismenny <danil@brandymint.ru>
2025-01-17 12:52:00 +0300

    Add NodesOrchestrator

---
+++ app/services/nodes_orchestrator.rb
@@ -0,0 +1,64 @@
+require 'rhelm/client'
+require 'tempfile'
+
+class NodesOrchestrator
+  RELEASE_PREFIX = 'anvil-'
+
+  attr_reader :cli
+
+  def initialize(node_id)
+    @node_id = node_id
+    @release = RELEASE_PREFIX + node_id
+    @cli = Rhelm::Client.new(
+      kubeconfig: ApplicationConfig.kubeconfig,
+      namespace: ApplicationConfig.namespace,
+      # program: '/path/to/a/specific/helm/binary'
+      # logger: Rhelm::Client::SimpleLogger
+    )
+  end
+
+  def install(values: {})
+    set = "ingress.hosts[0].host=#{node_id}.anvil-nodes.blockberry.cc"
+    with_values values do |values_path|
+      cli
+        .install(release, ApplicationConfig.chart_dir, create_namespace: true, set: set, values: values_path)
+        .run &method(:run_block)
+    end
+  end
+
+  def uninstall
+    cli.uninstall(release).run
+  end
+
+  def status
+    cli.status(release).run
+  end
+
+  private
+
+  attr_reader :release, :node_id
+
+  def logger
+    Rails.logger
+  end
+
+  def with_values(values, &block)
+    file = Tempfile.new(release + '-values.yaml')
+    file.write values.to_yaml
+    file.close
+    block.call file.path
+  ensure
+    file&.unlink
+  end
+
+  def run_block(lines, status)
+    if status == 0
+      logger.info("helm install worked great!")
+    elsif /timeout/im.match(lines)
+      raise MyTimeoutError, "helm install timed out, oh no!"
+    else
+      # Use the built-in error reporting code to get more details
+      report_failure(lines, status)
+    end
+  end
+end

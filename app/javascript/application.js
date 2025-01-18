5bdbb5b127b04e85d08a8013d18a938e6f05250a
Danil Pismenny <danil@brandymint.ru>
2025-01-18 21:49:27 +0300

    Fix bootstrap js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,26 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+
+const onReady = function() {
+  console.log("onReady emited");
+  var option = { delay: 3000 }
+  var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+  var toastList = toastElList.map(function (toastEl) {
+    var toast = new bootstrap.Toast(toastEl, option)
+    toast.show()
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+}
+
+document.addEventListener("turbo:load", onReady);
+
+// Чтобы флешки показывались на 422 после POST-запроса
+document.addEventListener("turbo:render", onReady);
+
+// Вынужден добавить из-за глюка в popper
+window.process = { env: { NODE_ENV: 'production' } };

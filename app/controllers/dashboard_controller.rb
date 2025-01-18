ce89432cb3f5a2ba0237844e23380e3a21575504
Danil Pismenny <danil@brandymint.ru>
2025-01-18 21:30:49 +0300

    Redirect from main dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,9 @@
 class DashboardController < ApplicationController
+  def index
+    if logged_in?
+      redirect_to tenant_root_url(subdomain: current_user.default_account.subdomain), allow_other_host: true
+    else
+      redirect_to new_session_url
+    end
+  end
 end

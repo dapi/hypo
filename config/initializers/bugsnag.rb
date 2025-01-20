1e210527a0fb9196f72adf524aadcc4805ce19e0
Danil Pismenny <danil@brandymint.ru>
2025-01-20 08:18:48 +0300

    lint

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,6 @@
 Bugsnag.configure do |config|
-  config.api_key = ENV.fetch('BUGSNAG_API_KEY', "7a189150a2ca438b5e62b3d906ddaf59")
-  config.app_version = AppVersion.format('%M.%m.%p')
+  config.api_key = ENV.fetch("BUGSNAG_API_KEY", "7a189150a2ca438b5e62b3d906ddaf59")
+  config.app_version = AppVersion.format("%M.%m.%p")
   config.notify_release_stages = %w[production]
   config.send_code = true
   config.send_environment = true

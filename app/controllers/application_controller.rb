aba26bce768bd09c9a8e09afe44d47a5d27c2117
Danil Pismenny <danil@brandymint.ru>
2025-01-21 13:10:09 +0300

    Support humanized errors in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  rescue_from HumanizedError, with: :humanized_error
+
+  private
+
+  def humanized_error(error)
+    @simple_layout = "simple" unless defined?(Admin) && is_a?(Admin::ApplicationController)
+    render "humanized_error", layout: @simple_layout, status: :forbidden, locals: { exception: error }
+  end
 end

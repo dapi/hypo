0fd6c17d3dccf97cd7f2b1a3a9f49dad6a4bda31
Danil Pismenny <danil@brandymint.ru>
2025-04-01 10:00:23 +0300

    Add locale switcher

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,7 +5,16 @@ class ProfileController < ApplicationController
 
   before_action :require_login
 
+  before_action do
+    @back_url = root_url if @back_url.blank?
+  end
+
   def show
     render locals: { user: current_user }
+  end
+
+  def update
+    current_user.update! params.require(:user).permit(:locale)
+    render :show, locals: { user: current_user }, notice: "Профиль изменён", status: :unprocessable_entity
   end
 end

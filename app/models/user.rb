a9cadf94d19203c8eae81062527e0c9b8df114ef
Danil Pismenny <danil@brandymint.ru>
2025-04-01 11:39:28 +0300

    Fix admin page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,11 @@ class User < ApplicationRecord
                             inverse_of: :owner
   has_many :memberships, dependent: :delete_all
   has_many :accounts, through: :memberships
-
   has_many :nodes, through: :accounts
 
   belongs_to :telegram_user, optional: true
 
-  delegate :first_name, :public_name, :telegram_nick, to: :telegram_user
+  delegate :first_name, :public_name, :telegram_nick, to: :telegram_user, allow_nil: true
 
   ROLES = %w[user superadmin developer].freeze
 

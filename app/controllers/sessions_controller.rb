c53ce8bd02c1f639bcad586149bd8ae27d7355bc
Danil Pismenny <danil@brandymint.ru>
2025-04-03 20:59:25 +0300

    Fix Будучи авторизованным, после закрытия ЛК, появляется страница о необходимости авторизации

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
   # rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    @back_url = :hide
     render locals: {
       user_session: EmailForm.new(email: session_email)
     }

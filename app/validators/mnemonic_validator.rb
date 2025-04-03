2786b5a2e8ae68cdf6417d7468d967a6d585ad47
Danil Pismenny <danil@brandymint.ru>
2025-04-03 16:55:52 +0300

    minor

--- app/validators/mnemonic_validator.rb
+++ app/validators/mnemonic_validator.rb
@@ -7,6 +7,6 @@ class MnemonicValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     BipMnemonic.checksum BipMnemonic.to_entropy mnemonic: value
   rescue IndexError, ArgumentError => exception
-    record.errors.add(attribute, options[:message], exception_message: exception.message)
+    record.errors.add(attribute, options[:message] || exception.message, exception_message: exception.message)
   end
 end

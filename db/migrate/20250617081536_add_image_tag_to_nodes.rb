61d81bc383b27868ce5ac62f08d5e53b2d29862d
Danil Pismenny <danil@brandymint.ru>
2025-06-17 13:10:27 +0300

    minor

--- db/migrate/20250617081536_add_image_tag_to_nodes.rb
+++ db/migrate/20250617081536_add_image_tag_to_nodes.rb
@@ -1,6 +1,6 @@
 class AddImageTagToNodes < ActiveRecord::Migration[8.0]
   def change
-    ImageTag.create!(tag: '0.0.1', description: 'anvil@0.53', repository: ApplicationConfig.anvil_default_repository)
+    ImageTag.create!(tag: '0.0.1', description: 'anvil@0.3.0', repository: ApplicationConfig.anvil_default_repository)
     it = ImageTag.create!(tag: '0.0.2', description: 'anvil@1.2.3-dev', repository: ApplicationConfig.anvil_default_repository, is_current: true)
     add_reference :nodes, :image_tag, foreign_key: true
 
